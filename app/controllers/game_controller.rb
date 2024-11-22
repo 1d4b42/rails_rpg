97f2b1b706468b0c2b1d68280a8b836d1edbfaa6
1d4b42 <fit4454@gmail.com>
2024-11-22 14:10:55 +0900

    #3 changed UI

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,5 +1,7 @@
 class GameController < ApplicationController
+
   def index
+    session[:game_end_flag] = false
     # セッションからプレイヤーと敵の状態を読み込む
     if session[:player_hp] && session[:player_mp] && session[:player_status]
       @player = Player.new(
@@ -7,7 +9,8 @@ class GameController < ApplicationController
         session[:player_is_bot], 
         session[:player_hp], 
         session[:player_mp], 
-        session[:player_status]
+        session[:player_status],
+        session[:player_dark_count]
       )
     else
       @player = Player.new("Player", false)
@@ -19,17 +22,20 @@ class GameController < ApplicationController
         session[:enemy_is_bot], 
         session[:enemy_hp], 
         session[:enemy_mp], 
-        session[:enemy_status]
+        session[:enemy_status],
+        session[:enemy_dark_count]
       )
     else
       @enemy = Player.new("Enemy", true)
     end
 
-    @message = "ゲームを開始しましょう！"
+    @message = "ゲームを開始しましょう！\n"
   end
 
   def action
-
+    if session[:game_end_flag]
+      return
+    end
 
     # セッションからプレイヤーと敵の状態を復元
     @player = Player.new(
@@ -37,7 +43,9 @@ class GameController < ApplicationController
       session[:player_is_bot] || false, 
       session[:player_hp] || 300, 
       session[:player_mp] || 50, 
-      session[:player_status] || "normal"
+      session[:player_status] || "normal",
+      session[:player_dark_count] || 0
+
     )
     
     @enemy = Player.new(
@@ -45,8 +53,11 @@ class GameController < ApplicationController
       session[:enemy_is_bot] || true, 
       session[:enemy_hp] || 300, 
       session[:enemy_mp] || 50, 
-      session[:enemy_status] || "normal"
+      session[:enemy_status] || "normal",
+      session[:enemy_dark_count] || 0
     )
+
+    @player.turn_start
   
     # プレイヤーのアクション名を取得
     action_name = params[:command]
@@ -62,40 +73,34 @@ class GameController < ApplicationController
   
     # MPが足りない場合
     if @action_result["attribute"] == "none"
-      @message = "MPが不足しています。別のコマンドを選んでください。"
-    else
-      # 敵にダメージを与える
-      @enemy.damage(@action_result["damage"], @action_result["attribute"])
+      while @action_result["attribute"] == "none"
+        @action_result = @player.action(action_name)
+      end
     end
+    
+    # 敵にダメージを与える
+    @enemy.damage(@action_result["damage"], @action_result["attribute"])
+    
 
-    @message = "#{@player.name}が#{@enemy.name}に#{@action_result["damage"]}ダメージ(状態: #{@action_result["attribute"]})を与えた"
+    if action_name!="healing_mp" && action_name!="healing_hp"
+      @message = "#{@player.name}が#{@enemy.name}に#{@action_result["damage"]}ダメージ(状態: #{@action_result["attribute"]})を与えた\n"
+    elsif action_name=="healing_mp"
+      @message = "#{@player.name}はMPを回復した! MPが#{@action_result["heal_mp"]}回復した!\n"
+    elsif action_name=="healing_hp"
+      @message = "#{@player.name}はHPを回復した! HPが#{@action_result["heal_hp"]}回復した!\n"
+    end
     
   
     # ターン終了処理
     @player.turn_end
-    
-
-    # 敵のアクションをランダムに選択
-    enemy_actions = ["punch", "kick", "fire", "blackhole"]
-    enemy_action = enemy_actions.sample  # ランダムで選択
-
-    # 敵のアクション実行
-    @enemy_action_result = @enemy.action(enemy_action)
-
-    # プレイヤーに敵のダメージを与える
-    if @enemy_action_result["attribute"] != "none"
-      @player.damage(@enemy_action_result["damage"], @enemy_action_result["attribute"])
-    end
-
-    @message = "#{@enemy.name}が#{@player.name}に#{@enemy_action_result["damage"]}ダメージ(状態: #{@enemy_action_result["attribute"]})を与えた"
-
-    @enemy.turn_end
 
     # 勝敗判定
     if @player.hp <= 0
-      @message = "ゲームオーバー！ 敵の勝利です。"
+      @message = "ゲームオーバー！ 敵の勝利\n"
+      session[:game_end_flag]=true
     elsif @enemy.hp <= 0
-      @message = "勝利！ 敵を倒しました。"
+      @message = "勝利！ 敵を倒しました\n"
+      session[:game_end_flag]=true
     end
   
     # セッションにプレイヤーと敵のインスタンス変数を保存
@@ -104,15 +109,101 @@ class GameController < ApplicationController
     session[:player_mp] = @player.mp
     session[:player_status] = @player.status
     session[:player_is_bot] = @player.is_bot
+    session[:player_dark_count] = @player.dark_count
   
     session[:enemy_name] = @enemy.name
     session[:enemy_hp] = @enemy.hp
     session[:enemy_mp] = @enemy.mp
     session[:enemy_status] = @enemy.status
     session[:enemy_is_bot] = @enemy.is_bot
+    session[:enemy_dark_count] = @enemy.dark_count
+
+    action_enemy
   
     render :index
   end
+
+  def action_enemy
+    # セッションからプレイヤーと敵の状態を復元
+    @player = Player.new(
+      session[:player_name] || "Player", 
+      session[:player_is_bot] || false, 
+      session[:player_hp] || 300, 
+      session[:player_mp] || 50, 
+      session[:player_status] || "normal",
+      session[:player_dark_count] || 0
+
+    )
+    
+    @enemy = Player.new(
+      session[:enemy_name] || "Enemy", 
+      session[:enemy_is_bot] || true, 
+      session[:enemy_hp] || 300, 
+      session[:enemy_mp] || 50, 
+      session[:enemy_status] || "normal",
+      session[:enemy_dark_count] || 0
+    )
+
+    @enemy.turn_start
+
+    # 敵のアクションをランダムに選択
+    enemy_actions = ["punch", "kick", "fire", "blackhole", "healing_mp", "healing_hp"]
+    enemy_action = enemy_actions.sample  # ランダムで選択
+
+    # 敵のアクション実行
+    @enemy_action_result = @enemy.action(enemy_action)
+
+    # MPが足りない場合
+    if @enemy_action_result["attribute"] == "none"
+      while @enemy_action_result["attribute"] == "none"
+        enemy_action = enemy_actions.sample  # ランダムで選択
+        @enemy_action_result = @enemy.action(enemy_action)
+      end
+    end
+
+    # プレイヤーに敵のダメージを与える
+    if @enemy_action_result["attribute"] != "none"
+      @player.damage(@enemy_action_result["damage"], @enemy_action_result["attribute"])
+    end
+
+    if enemy_action!="healing_mp" && enemy_action!="healing_hp"
+      @message += "#{@enemy.name}が#{@player.name}に#{@enemy_action_result["damage"]}ダメージ(状態: #{@enemy_action_result["attribute"]})を与えた\n"
+    elsif enemy_action=="healing_mp"
+      @message += "#{@enemy.name}はMPを回復した! MPが#{@enemy_action_result["heal_mp"]}回復した!\n"
+    elsif enemy_action=="healing_hp"
+      @message += "#{@enemy.name}はHPを回復した! HPが#{@enemy_action_result["heal_hp"]}回復した!\n"
+    end
+
+    @enemy.turn_end
+
+    # 勝敗判定
+    if @player.hp <= 0
+      @message = "ゲームオーバー！ 敵の勝利\n"
+      session[:game_end_flag]=true
+    elsif @enemy.hp <= 0
+      @message = "勝利！ 敵を倒しました\n"
+      session[:game_end_flag]=true
+    end
+  
+    # セッションにプレイヤーと敵のインスタンス変数を保存
+    session[:player_name] = @player.name
+    session[:player_hp] = @player.hp
+    session[:player_mp] = @player.mp
+    session[:player_status] = @player.status
+    session[:player_is_bot] = @player.is_bot
+    session[:player_dark_count] = @player.dark_count
+  
+    session[:enemy_name] = @enemy.name
+    session[:enemy_hp] = @enemy.hp
+    session[:enemy_mp] = @enemy.mp
+    session[:enemy_status] = @enemy.status
+    session[:enemy_is_bot] = @enemy.is_bot
+    session[:enemy_dark_count] = @enemy.dark_count
+
+  end
+
+    
+
   def reset
     # セッションのデータをクリアして新しいゲームを開始
     session[:player_name] = nil
@@ -120,12 +211,14 @@ class GameController < ApplicationController
     session[:player_mp] = nil
     session[:player_status] = nil
     session[:player_is_bot] = nil
+    session[:player_dark_count] = nil
   
     session[:enemy_name] = nil
     session[:enemy_hp] = nil
     session[:enemy_mp] = nil
     session[:enemy_status] = nil
     session[:enemy_is_bot] = nil
+    session[:enemy_dark_count] = nil
 
     # ゲームをリセットして最初から
     redirect_to game_index_path
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,20 +1,26 @@
 class Player
-  attr_accessor :name, :hp, :mp, :status, :is_bot
+  attr_accessor :name, :hp, :mp, :status, :is_bot, :dark_count, :max_hp, :max_mp
+
 
   ACTIONS = {
-    "punch" => { "attribute" => "normal", "damage" => 10, "cost" => 0 },
-    "kick" => { "attribute" => "normal", "damage" => 20, "cost" => 5 },
-    "fire" => { "attribute" => "fire", "damage" => 20, "cost" => 10 },
-    "blackhole" => { "attribute" => "dark", "damage" => 0, "cost" => 20 },
-    "insufficient_mp" => { "attribute" => "none", "damage" => 0, "cost" => 0 }
+    "punch" => { "attribute" => "normal", "damage" => 10, "heal_hp" => 0, "heal_mp" => 0, "cost" => 0 },
+    "kick" => { "attribute" => "normal", "damage" => 20, "heal_hp" => 0, "heal_mp" => 0, "cost" => 5 },
+    "fire" => { "attribute" => "fire", "damage" => 20, "heal_hp" => 0, "heal_mp" => 0, "cost" => 10 },
+    "blackhole" => { "attribute" => "dark", "damage" => 0, "heal_hp" => 0, "heal_mp" => 0, "cost" => 20 },
+    "insufficient_mp" => { "attribute" => "none", "damage" => 0, "heal_hp" => 0, "heal_mp" => 0, "cost" => 0 },
+    "healing_mp" => { "attribute" => "normal", "damage" => 0, "heal_hp" => 0, "heal_mp" => 20, "cost" => 0 },
+    "healing_hp" => { "attribute" => "normal", "damage" => 0, "heal_hp" => 50, "heal_mp" => 0, "cost" => 0 },
   }
 
-  def initialize(name = "noname", is_bot = false, hp = 300, mp = 50, status = "normal")
+  def initialize(name = "noname", is_bot = false, hp = 300, mp = 50, status = "normal", dark_count = 0)
     @name = name
     @hp = hp
     @mp = mp
     @status = status
     @is_bot = is_bot
+    @dark_count = dark_count
+    @max_hp = 300
+    @max_mp = 50
   end
 
   def damage(damage, status)
@@ -28,13 +34,29 @@ class Player
 
   def action(action_name)
     act = ACTIONS[action_name]
+    if action_name=="healing_mp"
+      @mp = @mp + act["heal_mp"]
+      @mp=100 if @mp>=100
+    elsif action_name=="healing_hp"
+      @hp = @hp + act["heal_hp"]
+      @hp=300 if @hp>=300
+    end
+    
     if @mp < act["cost"]
       return ACTIONS["insufficient_mp"]
     else
       @mp -= act["cost"]
       return act
     end
+    
   end
+  def turn_start
+    if @dark_count>=5
+      @status = "normal"
+      @dark_count = 0
+    end
+  end
+
 
   def turn_end
     case @status
@@ -43,6 +65,10 @@ class Player
     when "dark"
       rate = rand(1..100)
       @hp = 0 if rate >= 99
+      @dark_count = @dark_count + 1
+    end
+    if @hp<0
+      @hp=0
     end
   end
 end
